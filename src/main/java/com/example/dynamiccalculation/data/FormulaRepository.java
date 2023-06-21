ec7273e7fc932cb9b55e923f4deddc12656ebd10
Dolphin-in-river <311508@niuitmo.ru>
2023-06-21 22:53:21 +0300

    fix: code cleanup

--- src/main/java/com/example/dynamiccalculation/data/FormulaRepository.java
+++ src/main/java/com/example/dynamiccalculation/data/FormulaRepository.java
@@ -4,9 +4,9 @@ import io.spring.guides.gs_producing_web_service.Formula;
 import io.spring.guides.gs_producing_web_service.FormulaResponse;
 
 public interface FormulaRepository {
-    public int save(Formula result, double inputNumber);
+    int save(Formula result, double inputNumber);
 
-    public FormulaResponse getFormula(int id);
+    FormulaResponse getFormula(int id);
 
-    public FormulaResponse formulaExist(double number);
+    FormulaResponse formulaExist(double number);
 }
